42d6594cbb834f170ec50e7ae40d067eeba5a591
Andrii Yevchenko <and.yevchenko@gmail.com>
2024-10-21 21:29:52 +0300

    add sidebar

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -2,30 +2,26 @@ import './Sidebar.css';
 
 export function Sidebar() {
     const sidebar = document.createElement('aside');
-    header.classList.add('sidebar');
+    sidebar.classList.add('sidebar');
 
     sidebar.innerHTML = `
-        <div class="profile">
-            <img src="/src/assets/user/emma-roberts.jpg" alt="Emma Roberts" />
-            <div>
+        <button type="button" class="profile">
+            <img class="profile-img" src="/src/assets/user/emma-roberts.jpg" alt="Emma Roberts" />
+            <div class="profile-info">
                 <span class="profile-name">Emma Roberts</span>
                 <span class="profile-position">Sales manager</span>
             </div>
-        </div>
+        </button>
 
         <nav class="nav">
-            <ul class="nav-list">
-                <li class="nav-item"><i class="fa-solid fa-house"></i><span>Home</span></li>
-                <li class="nav-item"><i class="fa-solid fa-list-check"></i><span>Tasks</span></li>
-                <li class="nav-item"><i class="fa-solid fa-message"></i><span>Messages</span></li>
-                <li class="nav-item"><i class="fa-solid fa-users"></i><span>Team</span></li>
-            </ul>
+            <button type="button" class="nav-item"><i class="fa-solid fa-house"></i><span>Home</span></button>
+            <button type="button" class="nav-item"><i class="fa-solid fa-list-check"></i><span>Tasks</span></button>
+            <button type="button" class="nav-item"><i class="fa-solid fa-message"></i><span>Messages</span></button>
+            <button type="button" class="nav-item"><i class="fa-solid fa-user-group"></i></i><span>Team</span></button>
         </nav>
         
-        <div class="settings"><i class="fa-solid fa-gear"></i><span>Settings</span></div>
+        <button type="button" class="settings"><i class="fa-solid fa-gear"></i><span>Settings</span></button>
     `;
-
-
 
     return sidebar;
 }
--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { Header } from './components/Header/Header';
+import { Sidebar } from './components/Sidebar/Sidebar';
 import './style.css';
 
 const app = document.querySelector('#app');
 
 app.appendChild(Header());
+app.appendChild(Sidebar());
