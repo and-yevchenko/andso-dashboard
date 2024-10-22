e26ee68f43fd6858818d35725891ea0733ae83e9
Andrii Yevchenko <and.yevchenko@gmail.com>
2024-10-22 17:26:08 +0300

    separation of profile

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,4 +1,5 @@
 import { Nav } from '../Nav/Nav';
+import { Profile } from '../Profile/Profile';
 import './Sidebar.css';
 
 export function Sidebar() {
@@ -6,17 +7,11 @@ export function Sidebar() {
     sidebar.className = 'sidebar';
 
     sidebar.innerHTML = `
-        <button type="button" class="profile">
-            <img class="profile-img" src="/src/assets/user/emma-roberts.jpg" alt="Emma Roberts" />
-            <div class="profile-info">
-                <span class="profile-name">Emma Roberts</span>
-                <span class="profile-position">Sales manager</span>
-            </div>
-        </button>
-        
         <button type="button" class="settings"><i class="fa-solid fa-gear"></i><span>Settings</span></button>
     `;
 
-    sidebar.appendChild(Nav());
+    sidebar.prepend(Nav());
+    sidebar.prepend(Profile());
+
     return sidebar;
 }
