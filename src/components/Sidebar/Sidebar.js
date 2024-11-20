4e3a83287bed19ebcb28f2c5574390e73828a78a
Andrii Yevchenko <and.yevchenko@gmail.com>
2024-11-20 23:12:09 +0200

    Update Sidebar.js

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -6,12 +6,14 @@ export function Sidebar() {
     const sidebar = document.createElement('aside');
     sidebar.className = 'sidebar';
 
+    const nav = new Nav()
+    const profile = new Profile()
+
     sidebar.innerHTML = `
+        ${nav.outerHTML}
+        ${profile.outerHTML}
         <button type="button" class="settings"><i class="fa-solid fa-gear"></i><span>Settings</span></button>
     `;
-
-    sidebar.prepend(Nav());
-    sidebar.prepend(Profile());
 
     return sidebar;
 }
